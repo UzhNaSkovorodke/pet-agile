568b1a8247f75271d6bcfeceaf5a846805143214
UzhNaSkovorodke <piratt.kiri@gmail.com>
2023-03-30 21:51:00 +0700

    rework-kanbanelement

--- src/components/KanbanComponents/KanbanBody/KanbanBody.tsx
+++ src/components/KanbanComponents/KanbanBody/KanbanBody.tsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 
-//import {useSelector} from 'react-redux';
-//import {RootState} from '../../../store/store';
-import KanbanElement from '../KanbanElement/KanbanElement';
+import {RootState} from '../../../store/store';
 
 import './KanbanBody.scss';
 
@@ -13,45 +12,38 @@ interface IKanbanBodyProps {
 const elementList: string[] = ['backlog', 'process', 'done'];
 
 const KanbanBody: React.FunctionComponent<IKanbanBodyProps> = ({setIsActiveModal}) => {
-  const [tasks, setTasks] = useState<any>('2');
+  const [tasks, setTasks] = useState<any>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
 
   const data: object = {};
-  //ВАЖНО КОРОЧЕ ПОПРОБУЙ ПОВТОРОИТЬ В ТОЧНОСТИ КАК У ULBI И ЕСЛИ ПОЛУЧИТСЯ ТО ПЕРЕДЕЛАЙ
+
   const getTasks = async () => {
     setIsLoading(true);
     try {
-      const {data} = await axios.get('http://localhost:5000/get-tasks');
+      const {data} = await axios.get('http://localhost:5000/');
       setTasks(data);
       setIsError(false);
     } catch (error) {
       setIsError(true);
     } finally {
       setIsLoading(false);
-      console.log('finally');
     }
   };
+
   useEffect(() => {
     getTasks();
   }, []);
-  //const TicketsState = useSelector((state: RootState) => state.ticketList.ticketsList);
-  //const tickets = list;
+
+  const TicketsState = useSelector((state: RootState) => state.ticketList.ticketsList);
+  console.log(TicketsState);
 
   if (isError) {
     return <div>Ошибка получения данных</div>;
   }
   if (isLoading) {
     return <div>Загружаются данные...</div>;
-  }
-  return (
-    <div className="kanban-body">
-      {tasks}
-      {/* {elementList.map((item: string) => (
-        <KanbanElement key={Date.now() + Math.random()} setIsActiveModal={setIsActiveModal} typeOfElement={item} tickets={tasks} />
-      ))} */}
-    </div>
-  );
+  } else return <div className="kanban-body"></div>;
 };
 
 export default KanbanBody;
