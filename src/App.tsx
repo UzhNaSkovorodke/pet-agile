34cc6ee4cc5e6b9e3fe627b8c042f4b02df47a27
UzhNaSkovorodke <piratt.kiri@gmail.com>
2023-04-10 22:36:46 +0700

    redev

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.scss'
-import Kanban from './pages/Kanban'
+import './App.scss';
+import Kanban from './pages/Kanban';
 
 function App() {
-	return (
-		<div className="App">
-			<Kanban />
-		</div>
-	)
+  return (
+    <div className="App">
+      <Kanban />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Kanban.tsx
+++ src/pages/Kanban.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 
-import KanbanBoard from '../modules/KanbanBoard/KanbanBoard';
-import Navigation from '../modules/Navigation/Navigation';
-
 interface IKanbanProps {}
 
 const Kanban: React.FunctionComponent<IKanbanProps> = props => {
   return (
     <>
-      <Navigation />
-      <KanbanBoard />
+      <div className=""></div>
     </>
   );
 };
